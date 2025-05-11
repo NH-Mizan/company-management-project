8fd9435433b72a1f9ffa583b65b04ba3c5106739
nh-mizan <nhmizan999@gmail.com>
2025-05-11 19:02:47 +0600

    banner section complete

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{ items:1 },
+            600:{ items:1 },
+            1000:{ items:1 }
+        }
+    });
+  });
+  

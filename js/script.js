d40a6c555245ba1d94010bdff18750ad4e877b31
nh-mizan <nhmizan999@gmail.com>
2025-05-12 11:49:25 +0600

    product card section  complete

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         autoplayTimeout: 3500,
         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
         responsive:{
-            0:{ items:1 },
+            0:{ items:1, nav:false },
             600:{ items:1 },
             1000:{ items:1 }
         }
